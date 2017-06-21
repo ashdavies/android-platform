a74a6ebd940cdfde7112785b0d82f406c52b22db
Ash Davies <ash.davies@gmail.com>
2017-06-21 15:59:39 +0200

    include git sync and pgp verify

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,2 +1,5 @@
 echo "y" | repo init -u https://android.googlesource.com/platform/manifest
+repo sync --current-branch --optimized-fetch --quiet --jobs=4
 
+gpg --import google.asc
+git tag -v $(git describe --abbrev=0 --tags)
